eb9c7af087ad48ef9a468a98122fb8da42e736c1
StackNet <121740972+Sahasra-Kesara@users.noreply.github.com>
2026-07-02 03:25:05 +0530

    Hided contact number

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -36,7 +36,7 @@ export default function ContactPage() {
               </a>
             </div>
             
-            <div className="bg-white p-8 border border-gray-200 shadow-sm">
+            {/* <div className="bg-white p-8 border border-gray-200 shadow-sm">
               <div className="w-10 h-10 bg-teal-50 flex items-center justify-center mb-6">
                 <svg className="w-5 h-5 text-teal-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="square" strokeLinejoin="miter" strokeWidth={1.5} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
@@ -47,7 +47,7 @@ export default function ContactPage() {
               <p className="text-teal-600 font-medium">
                 +94 740378425
               </p>
-            </div>
+            </div> */}
             
             <div className="bg-white p-8 border border-gray-200 shadow-sm">
               <div className="w-10 h-10 bg-teal-50 flex items-center justify-center mb-6">
